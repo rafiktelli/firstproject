05bf540aa06763aa1be9ca04a61e7b70a51006e7
TELLI Mohamed Khoudja <hm_telli@esi.dz>
2022-12-26 17:41:21 +0100

    ss

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ export default function App() {
     
     <View style={styles.global}>
     <WelcomeScreen />
-    <Text> HIIIIIIIIIi</Text>
+    <Text> HIIIIIIIII</Text>
     </View>
      
   );
